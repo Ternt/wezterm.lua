c9dbe35875f50284f7ead2816037bd250e157023
Ternt <kyle.pham202@gmail.com>
2024-12-07 17:06:59 -0500

    default to command prompt

--- wezterm.lua
+++ wezterm.lua
@@ -113,12 +113,41 @@ end)
 local platform = get_platform()
 if platform.is_win32 then
     config.default_prog = {
-        'powershell.exe',
-        '-Nologo',
+        'cmd.exe',
+        '/k C:\\Users\\"Thinh Pham"\\Dev\\setup.bat'
     }
     config.default_cwd  = os.getenv("USERPROFILE") .. "\\dev\\projects\\"
 end
 
+-- local launch_menu = {}
+-- if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+--   table.insert(launch_menu, {
+--     label = 'PowerShell',
+--     args = { 'powershell.exe', '-NoLogo' },
+--   })
+--
+--   -- Find installed visual studio version(s) and add their compilation
+--   -- environment command prompts to the menu
+--   for _, vsvers in
+--     ipairs(
+--       wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
+--     )
+--   do
+--     local year = vsvers:gsub('Microsoft Visual Studio/', '')
+--     table.insert(launch_menu, {
+--       label = 'x64 Native Tools VS ' .. year,
+--       args = {
+--         'cmd.exe',
+--         '/k',
+--         'C:/Program Files (x86)/'
+--           .. vsvers
+--           .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
+--       },
+--     })
+--   end
+-- end
+--
+-- config.launch_menu = launch_menu;
 
 -- domains
 if platform.is_win32 then
