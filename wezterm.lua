5bd4b0cfcc453ede690b51eaf8ca5eba08ef9855
Ternt <kyle.pham202@gmail.com>
2025-01-18 21:28:39 -0500

    simpler wezterm config

--- wezterm.lua
+++ wezterm.lua
@@ -20,19 +20,6 @@ local function get_platform()
     }
 end
 
-
--- startup gui
-wezterm.on("gui-startup", function()
-    local mux = wezterm.mux
-    local tab, pane, mux_window = mux.spawn_window {
-        workspace = 'default'
-    }
-
-    local window = mux_window:gui_window()
-
-    window:toggle_fullscreen()
-end)
-
 -- show current active key table in the status area
 wezterm.on('update-right-status', function(window)
     local name = window:active_key_table()
@@ -82,64 +69,31 @@ wezterm.on('window-resized', function(window)
     window:set_config_overrides(overrides)
 end)
 
--- editing in vim
-wezterm.on("open_in_vim", function(window, pane)
-    local platform = get_platform()
-
-    local file = ""
-    if platform.is_win32 then
-        file = os.getenv("TEMP") .. "\\wezterm_buf"
-    end
-
-	local handle = io.open(file, "w")
-    if not handle then
-        wezterm.log_error("failed to open file")
-        return
-    end
-
-	handle:write(pane:get_lines_as_text(3000))
-	handle:close()
-    window:perform_action(
-        wezterm.action({
-            SpawnCommandInNewTab = {
-                args = { "nvim", file, "-c", "call cursor(3000,0)" }
-            },
-        }),
-        pane
-    )
-end)
-
-
 local platform = get_platform()
+local launch_menu = {}
 if platform.is_win32 then
+    config.exit_behavior = "Close"
+    config.default_cwd  = os.getenv("USERPROFILE") .. "\\source\\repos\\"
     config.default_prog = {
-        'cmd.exe',
-        '/k C:\\Users\\"Thinh Pham"\\Dev\\setup.bat'
+        'powershell.exe',
+        'powershell.exe', '-NoLogo', '-NoExit', '-File "$env:DEVENV\\setupshell.ps1"'
     }
-    config.default_cwd  = os.getenv("USERPROFILE") .. "\\dev\\projects\\"
-end
 
--- domains
-if platform.is_win32 then
-    config.wsl_domains = {
-        {
-            name = 'WSL:Arch',
-            distribution = 'Arch',
-            default_cwd = '/home/tpham',
-        },
-        {
-            name = 'WSL:Ubuntu',
-            distribution = 'Ubuntu',
-            default_cwd = '/home/tpham',
-        },
-    }
+    table.insert(launch_menu, {
+        label = 'cmd.exe',
+        args  = '/k C:\\Users\\"Thinh Pham"\\source\\setup.bat'
+    })
+
+    table.insert(launch_menu, {
+        label = 'powershell.exe',
+        args = { 'powershell.exe', '-NoLogo'}
+    })
 end
 
 -- general
 config.automatically_reload_config = true
-config.exit_behavior = "CloseOnCleanExit"
 config.exit_behavior_messaging = "Verbose"
-config.scrollback_lines = 20000
+config.scrollback_lines = 100000
 
 
 -- rendering
@@ -153,35 +107,61 @@ end
 
 -- color
 config.term = "xterm-256color"
-config.colors = {
-    tab_bar = {
-        background = "#000000",
-        active_tab = {
-            bg_color = "#000000",
-            fg_color = "#C0C0C0",
-        },
-        inactive_tab = {
-            bg_color = "#000000",
-            fg_color = "#303030",
-        },
-        inactive_tab_hover = {
-            bg_color = "#000000",
-            fg_color = "#C0C0C0",
-        },
+config.color_scheme = 'Alacritty'
+config.color_schemes = {
+    ['Alacritty'] = {
+        background = "#181818",
+        cursor_bg = '#deac49',
+        cursor_fg = 'black',
+
+        cursor_border = '#ceac49',
+        tab_bar = {
+            background = "#1d1d1d",
+            active_tab = {
+                bg_color = "#181818",
+                fg_color = "#deac49",
+            },
+            inactive_tab = {
+                bg_color = "#181818",
+                fg_color = "#303030",
+            },
+            inactive_tab_hover = {
+                bg_color = "#181818",
+                fg_color = "#c0c0c0",
+            },
+        }
     }
+}
+
+config.inactive_pane_hsb = {
+    saturation = 0.1,
 }
 
 
 -- fonts
-config.font_size = 12.0
+config.font_size = 10.0
 config.font = wezterm.font({
-    family = "Cascadia Mono",
-    weight = "Medium"
+    family = "Consolas",
+    weight = "Regular"
 })
 
 
 -- cursor
-config.default_cursor_style = "BlinkingBlock"
+config.default_cursor_style = "SteadyBlock"
+
+
+-- ui
+if platform.is_win32 then
+    config.ui_key_cap_rendering = "Emacs"
+elseif platform.is_macOS then
+    config.ui_key_cap_rendering = "AppleSymbols"
+elseif platform.is_linux then
+    config.ui_key_cap_rendering = "UnixLong"
+end
+config.command_palette_font_size = 10.0
+config.command_palette_bg_color = "rgba(0.11, 0.11, 0.11, 0.2)"
+config.char_select_font_size = 10.0
+config.char_select_bg_color = "rgba(0.11, 0.11, 0.11, 0.2)"
 
 
 -- tabs
@@ -194,8 +174,7 @@ config.show_new_tab_button_in_tab_bar = false
 config.switch_to_last_active_tab_when_closing_tab = true
 
 -- windows
-config.window_background_opacity = 0.9
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.adjust_window_size_when_changing_font_size = false
 
@@ -204,21 +183,16 @@ config.window_padding = {
     top = '0.5cell', bottom = '0.5cell',
 }
 
-config.inactive_pane_hsb = {
-    saturation = 0.1,
-    brightness = 0.2,
-}
-
 
 -- key bindings
 config.use_dead_keys = false
 config.disable_default_key_bindings = true
-config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.leader = { key = 'A', mods = 'CTRL', timeout_milliseconds = 400 }
 
 local function focus_pane(key, direction)
     return {
         key = key,
-        mods = "CTRL",
+        mods = "CTRL|SHIFT",
         action = action.ActivatePaneDirection(direction),
     }
 end
@@ -239,41 +213,59 @@ local function split_pane(key, direction)
     }
 end
 
--- project selection
-local function get_project_dirs(path)
-    local projects = {}
-    for _, project_dir in ipairs(wezterm.glob(path .. '/*')) do
-        table.insert(projects, project_dir)
-    end
-    return projects
-end
+-- -- project selection
+-- local function get_project_dirs(path, file)
+--     local projects = {}
+--     if file == nil then
+--         for _, project_dir in ipairs(wezterm.glob(path .. '/*')) do
+--             table.insert(projects, project_dir)
+--         end
+--     else
+--         for _, glob in ipairs(file) do
+--             for _, project_dir in ipairs(wezterm.glob(path .. '/' .. glob)) do
+--                 table.insert(projects, project_dir)
+--             end
+--         end
+--     end
+--     return projects
+-- end
 
-local function choose_project()
-    local choices = {}
-    local project_path = os.getenv("USERPROFILE") .. "\\dev\\projects\\"
-    for _, value in pairs(get_project_dirs(project_path)) do
-        table.insert(choices, { label = value })
-    end
-
-    local lua_conf_path = os.getenv("USERPROFILE") .. "\\AppData\\local\\nvim\\lua"
-    for _, value in pairs(get_project_dirs(lua_conf_path)) do
-        table.insert(choices, { label = value })
-    end
-
-    return wezterm.action.InputSelector({
-        title = "Projects",
-        choices = choices,
-        fuzzy = false,
-        action = wezterm.action_callback(function(window, pane, id, label)
-            if not label then return end
-
-            window:perform_action(action.SwitchToWorkspace {
-                name = label:match("([^/]+)$"),
-                spawn = { cwd = label },
-            }, pane)
-        end),
-    })
-end
+-- local function choose_project()
+--     local choices = {}
+--     local project_path = os.getenv("USERPROFILE") .. "\\source\\repos\\"
+--     for _, value in pairs(get_project_dirs(project_path, {"toy-renderer", "creo"})) do
+--         table.insert(choices, { label = value })
+--     end
+--
+--     local lua_conf_path = os.getenv("USERPROFILE") .. "\\AppData\\local\\nvim\\lua"
+--     for _, value in pairs(get_project_dirs(lua_conf_path)) do
+--         table.insert(choices, { label = value })
+--     end
+--
+--     local wez_conf_path = "C:\\Program Files"
+--     for _, value in pairs(get_project_dirs(wez_conf_path, {'Wezterm'})) do
+--         table.insert(choices, { label = value })
+--     end
+--
+--     local forcoder_conf_path = "C:\\Program Files (x86)"
+--     for _, value in pairs(get_project_dirs(forcoder_conf_path, {'4coder'})) do
+--         table.insert(choices, { label = value })
+--     end
+--
+--     return wezterm.action.InputSelector({
+--         title = "Projects",
+--         choices = choices,
+--         fuzzy = false,
+--         action = wezterm.action_callback(function(window, pane, id, label)
+--             if not label then return end
+--
+--             window:perform_action(action.SwitchToWorkspace {
+--                 name = label:match("([^/]+)$"),
+--                 spawn = { cwd = label },
+--             }, pane)
+--         end),
+--     })
+-- end
 
 
 config.keys = {
@@ -283,11 +275,66 @@ config.keys = {
     focus_pane('l', "Right"),
     focus_pane('k', "Up"),
     focus_pane('j', "Down"),
-
-    { key = 'T', mods = 'CTRL|SHIFT', action = action.SpawnTab 'CurrentPaneDomain' },
-    { key = 'D', mods = 'CTRL|SHIFT', action = action.ShowDebugOverlay },
-    { key = 'x', mods = 'LEADER', action = action.ActivateCopyMode },
-    { key = 'p', mods = 'LEADER', action = choose_project() },
+    {
+        key = 'T',
+        mods = 'CTRL|SHIFT',
+        action = action.SpawnTab 'CurrentPaneDomain'
+    },
+    {
+        key = 'w',
+        mods = 'CTRL',
+        action = action.CloseCurrentTab {confirm=true}
+    },
+    {
+        key = 'D',
+        mods = 'CTRL|SHIFT',
+        action = action.ShowDebugOverlay
+    },
+    {
+        key = 'x',
+        mods = 'LEADER',
+        action = action.ActivateCopyMode
+    },
+    {
+        key = 'f',
+        mods = 'CTRL',
+        action = action.Search { CaseInSensitiveString = "" }
+    },
+    {
+        key = 'V',
+        mods = 'CTRL',
+        action = action.PasteFrom 'Clipboard'
+    },
+    {
+        key = 'V',
+        mods = 'CTRL',
+        action = action.PasteFrom 'PrimarySelection'
+    },
+    {
+        key = '-',
+        mods = 'CTRL',
+        action = action.DecreaseFontSize
+    },
+    {
+        key = '=',
+        mods = 'CTRL',
+        action = action.IncreaseFontSize
+    },
+    {
+        key = 'DownArrow',
+        mods = 'SUPER',
+        action = action.Hide
+    },
+    {
+        key = 'UpArrow',
+        mods = 'SUPER',
+        action = action.Show
+    },
+    {
+        key = ' ',
+        mods = 'SHIFT|CTRL',
+        action = wezterm.action.QuickSelect
+    },
     {
         key = 'r',
         mods = 'LEADER',
@@ -297,18 +344,37 @@ config.keys = {
         }
     },
     {
+        key = 'Enter',
+        mods = 'ALT',
+        action = action.ToggleFullScreen
+    },
+    {
+        key = 'n',
+        mods = 'CTRL|SHIFT',
+        action = action.SpawnWindow
+    },
+    {
+        key = 'P',
+        mods = 'CTRL|SHIFT',
+        action = action.ActivateCommandPalette
+    },
+    {
+        key = 'U',
+        mods = 'CTRL|SHIFT',
+        action = action.CharSelect
+    },
+    {
+        key = 'Z',
+        mods = 'CTRL|SHIFT',
+        action = action.TogglePaneZoomState
+    },
+--     { key = 'p', mods = 'LEADER', action = choose_project() },
+    {
         key = 'w',
         mods = 'LEADER',
         action = action.ActivateKeyTable({
             name = 'switch_workspace',
             one_shot = false,
-        }),
-    },
-    {
-        key = 't',
-        mods = 'LEADER',
-        action = action.ActivateKeyTable({
-            name = 'toggle_fullscreen',
         }),
     },
 }
@@ -325,7 +391,6 @@ for i = 1, 8 do
         action = action.ActivateTab(i - 1),
     })
 end
-
 config.key_tables = {
     resize_pane = {
         { key = 'LeftArrow', mods = 'NONE', action = action.AdjustPaneSize { 'Left', 1 } },
@@ -342,31 +407,31 @@ config.key_tables = {
 
         { key = 'Escape', action = 'PopKeyTable' },
     },
-    copy_mode = {
-        { key = 'h', mods = 'NONE', action = action.CopyMode 'MoveLeft' },
-        { key = 'j', mods = 'NONE', action = action.CopyMode 'MoveDown' },
-        { key = 'k', mods = 'NONE', action = action.CopyMode 'MoveUp' },
-        { key = 'l', mods = 'NONE', action = action.CopyMode 'MoveRight' },
-        { key = 'b', mods = 'NONE', action = action.CopyMode 'MoveBackwardWord' },
-        { key = 'g', mods = 'NONE', action = action.CopyMode 'MoveToScrollbackBottom' },
-        { key = 'v', mods = 'NONE', action = action.CopyMode { SetSelectionMode = 'Cell' } },
-        { key = 'v', mods = 'SHIFT', action = action.CopyMode { SetSelectionMode = 'Line' } },
-        {
-            key = 'y',
-            mods = 'NONE',
-            action = action.Multiple {
-                { CopyTo = 'ClipboardAndPrimarySelection' },
-                { CopyMode = 'Close' },
-            },
-        },
-        {
-            key = 'Escape',
-            mods = 'NONE',
-            action = action.Multiple {
-                { CopyMode = 'Close' },
-            }
-        },
-    },
+--     copy_mode = {
+--         { key = 'h', mods = 'NONE', action = action.CopyMode 'MoveLeft' },
+--         { key = 'j', mods = 'NONE', action = action.CopyMode 'MoveDown' },
+--         { key = 'k', mods = 'NONE', action = action.CopyMode 'MoveUp' },
+--         { key = 'l', mods = 'NONE', action = action.CopyMode 'MoveRight' },
+--         { key = 'b', mods = 'NONE', action = action.CopyMode 'MoveBackwardWord' },
+--         { key = 'g', mods = 'NONE', action = action.CopyMode 'MoveToScrollbackBottom' },
+--         { key = 'v', mods = 'NONE', action = action.CopyMode { SetSelectionMode = 'Cell' } },
+--         { key = 'v', mods = 'SHIFT', action = action.CopyMode { SetSelectionMode = 'Line' } },
+--         {
+--             key = 'y',
+--             mods = 'NONE',
+--             action = action.Multiple {
+--                 { CopyTo = 'ClipboardAndPrimarySelection' },
+--                 { CopyMode = 'Close' },
+--             },
+--         },
+--         {
+--             key = 'Escape',
+--             mods = 'NONE',
+--             action = action.Multiple {
+--                 { CopyMode = 'Close' },
+--             }
+--         },
+--     },
     switch_workspace = {
         { key = 'k', mods = 'NONE', action = action.SwitchWorkspaceRelative(1) },
         { key = 'j', mods = 'NONE', action = action.SwitchWorkspaceRelative(-1) },
@@ -380,13 +445,6 @@ config.key_tables = {
         },
         { key = 'Escape', action = 'PopKeyTable' }
     },
-    toggle_fullscreen = {
-        {
-            key = 'f',
-            mods = 'NONE',
-            action = action.ToggleFullScreen,
-        },
-    }
 }
 
 
